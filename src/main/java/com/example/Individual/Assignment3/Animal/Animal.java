1fe6ae027f3720f6a9b61f487ebd8a4395631d06
Darlenne Noemi Mosqueda <dnmosquedaa@uncg.edu>
2025-06-02 10:29:39 -0400

    update

--- src/main/java/com/example/Individual/Assignment3/Animal/Animal.java
+++ src/main/java/com/example/Individual/Assignment3/Animal/Animal.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "animal")
+@Table(name = "animals")
 public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
